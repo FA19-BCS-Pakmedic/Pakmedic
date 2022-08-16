835639c02c43d2a1fd6803a9b84dfdcbbe30e9fa
Moeed <63580699+MoeedthePessimist@users.noreply.github.com>
2022-08-16 11:05:41 +0500

    Development (#15)
    * test commit
    
    * Enable Actions Workflow (#1) (#4)
    
    * Enable Actions Workflow (#1) (#5)
    
    * Moeed (#7)
    
    * changed
    
    * test
    
    * login/signup, boilerplate (#14)
    
    * changed
    
    * test
    
    * added helper file for git command
    
    * added versions file
    
    * changed react native version
    
    * deleted yml files
    
    * login and signup done for basic, google and facebook along with the boiler plate code
    
    Co-authored-by: Ali Hamza <85640669+Plasteredpeak@users.noreply.github.com>

---
+++ test/src/App.js
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
+import FacebookLogin from "react-facebook-login";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
+import { findAllByDisplayValue } from "@testing-library/react";
+
+function App() {
+  const googleLoginSuccess = async (data) => {
+    console.log(jwtDecode(data.credential));
+    const { email, email_verified } = jwtDecode(data.credential);
+
+    if (email_verified) {
+      const response = await axios.post(
+        "http://localhost:8000/auth/google",
+        {
+          credentials: data.credential,
+          role: "patient",
+          password: "hehe1234",
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      console.log(response);
+    }
+  };
+
+  const googleLoginFailure = async (err) => {
+    console.log(err);
+  };
+
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
+
+  return (
+    <div className="App">
+      <GoogleLogin
+        onSuccess={(data) => {
+          googleLoginSuccess(data);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+      <FacebookLogin
+        appId="2143874102457540"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-facebook-button-class"
+        icon="fa-facebook"
+      />
+    </div>
+  );
+}
+
+export default App;
