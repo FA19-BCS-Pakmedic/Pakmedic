50bf3130309d8fa4d5656782b3fe79345e4bee1f
Ali Hamza <hamza120025@gmail.com>
2022-11-25 18:15:53 +0500

    added dimensions for screens with different sizes

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -5,18 +5,16 @@ import {NavigationContainer} from '@react-navigation/native';
 
 // import root navigation
 import RootNavigation from './src/setup/navigation/root.navigation';
-import ForgotPassword from './src/screens/shared/Authentication/Forgot-password';
-import OtpVerification from './src/screens/shared/Authentication/OTP-verification';
-import SetNewPassword from './src/screens/shared/Authentication/Set-new-password';
+import SupportHome from './src/screens/shared/Support-communities/Home';
 
 const App = () => {
   return (
-    // <NavigationContainer>
-    //   <RootNavigation />
-    // </NavigationContainer>
-    <SafeAreaView style={styles.root}>
-      <ForgotPassword />
-    </SafeAreaView>
+    <NavigationContainer>
+      <RootNavigation />
+    </NavigationContainer>
+    // <SafeAreaView style={styles.root}>
+    //   <SupportHome />
+    // </SafeAreaView>
   );
 };
 
